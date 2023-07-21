6421c376d462579b80e4dbcc2601c2e2f972c8c2
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-21 22:16:55 +0700

    Organize Redux to Actions, Constants and Reducers

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
