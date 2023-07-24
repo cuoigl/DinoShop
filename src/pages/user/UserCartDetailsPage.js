a7682081fb7d7b9bda2812f554f24686f1135346
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-24 22:25:51 +0700

    User Info On Cart Details Page

--- src/pages/user/UserCartDetailsPage.js
+++ src/pages/user/UserCartDetailsPage.js
@@ -3,21 +3,34 @@ import UserCartDetailsPageComponent from "./components/UserCartDetailsPageCompon
 import { useSelector, useDispatch } from "react-redux";
 import { addToCart, removeFromCart } from "../../redux/actions/cartActions";
 
+import axios from "axios";
+
 const UserCartDetailsPage = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
   const itemsCount = useSelector((state) => state.cart.itemsCount);
   const cartSubtotal = useSelector((state) => state.cart.cartSubtotal);
+  const userInfo = useSelector((state) => state.userRegisterLogin.userInfo);
 
   const reduxDispatch = useDispatch();
+
+  const getUser = async () => {
+    const { data } = await axios.get(
+      "http://localhost:3000/api/users/profile/" + userInfo._id,
+      { withCredentials: true }
+    );
+    return data;
+  };
 
   return (
     <UserCartDetailsPageComponent
       cartItems={cartItems}
       itemsCount={itemsCount}
       cartSubtotal={cartSubtotal}
+      userInfo={userInfo}
       addToCart={addToCart}
       removeFromCart={removeFromCart}
       reduxDispatch={reduxDispatch}
+      getUser={getUser}
     />
   );
 };
