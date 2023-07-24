0bc5579dfbaf4f55beca574701e8df4b6d99a04d
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-24 16:06:43 +0700

    Make Final Cart Items Calculations

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -2,7 +2,9 @@ import * as actionTypes from "../constants/cartConstants";
 import axios from "axios";
 
 export const addToCart = (productId, quantity) => async (dispatch) => {
-  const { data } = await axios.get(`/api/products/get-one/${productId}`);
+  const { data } = await axios.get(
+    `http://localhost:3000/api/products/get-one/${productId}`
+  );
   dispatch({
     type: actionTypes.ADD_TO_CART,
     payload: {
