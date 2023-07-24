68db9d08ebcb0b1b9b3ed7990fd10dc712b47488
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-24 16:09:10 +0700

    Store Cart Items in Local Storage

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -1,19 +1,22 @@
 import * as actionTypes from "../constants/cartConstants";
 import axios from "axios";
 
-export const addToCart = (productId, quantity) => async (dispatch) => {
-  const { data } = await axios.get(
-    `http://localhost:3000/api/products/get-one/${productId}`
-  );
-  dispatch({
-    type: actionTypes.ADD_TO_CART,
-    payload: {
-      productID: data._id,
-      name: data.name,
-      price: data.price,
-      image: data.images[0] ?? null,
-      count: data.count,
-      quantity,
-    },
-  });
-};
+export const addToCart =
+  (productId, quantity) => async (dispatch, getState) => {
+    const { data } = await axios.get(
+      `http://localhost:3000/api/products/get-one/${productId}`
+    );
+    dispatch({
+      type: actionTypes.ADD_TO_CART,
+      payload: {
+        productID: data._id,
+        name: data.name,
+        price: data.price,
+        image: data.images[0] ?? null,
+        count: data.count,
+        quantity,
+      },
+    });
+
+    localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
+  };
