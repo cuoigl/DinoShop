2af8732a5a567d452032450f3c192e22c4ba7682
Cuoi <cuoi@Nguyens-MBP.lan>
2023-07-25 20:29:51 +0700

    Upload Images

--- src/pages/admin/AdminEditProductPage.js
+++ src/pages/admin/AdminEditProductPage.js
@@ -24,6 +24,19 @@ const updateProductApiRequest = async (productId, formInputs) => {
   return data;
 };
 
+const uploadHandler = async (images, productId) => {
+  const formData = new FormData();
+
+  Array.from(images).forEach((image) => {
+    formData.append("images", image);
+  });
+  await axios.post(
+    "http://localhost:3000/api/products/admin/upload?productId=" + productId,
+    formData,
+    { withCredentials: true }
+  );
+};
+
 const AdminEditProductPage = () => {
   const { categories } = useSelector((state) => state.getCategories);
 
@@ -45,7 +58,9 @@ const AdminEditProductPage = () => {
       reduxDispatch={reduxDispatch}
       saveAttributeToCatDoc={saveAttributeToCatDoc}
       imageDeleteHandler={imageDeleteHandler}
+      uploadHandler={uploadHandler}
     />
   );
 };
+
 export default AdminEditProductPage;
