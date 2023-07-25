530b313e8de0f8d804671a4e3eeb5cb77343ac11
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-25 08:59:59 +0700

    List All Cart Items On User Order Details Page

--- src/pages/user/components/UserCartDetailsPageComponent.js
+++ src/pages/user/components/UserCartDetailsPageComponent.js
@@ -8,30 +8,108 @@ import {
   Button,
 } from "react-bootstrap";
 import CartItemComponent from "../../../components/CartItemComponent";
+
 import { useEffect, useState } from "react";
 
-const UserOrderDetailsPageComponent = ({ userInfo, getUser }) => {
-  const [userAddress, setUserAddress] = useState({});
+import { useNavigate } from "react-router-dom";
+
+const UserCartDetailsPageComponent = ({
+  cartItems,
+  itemsCount,
+  cartSubtotal,
+  userInfo,
+  addToCart,
+  removeFromCart,
+  reduxDispatch,
+  getUser,
+  createOrder,
+}) => {
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [userAddress, setUserAddress] = useState(false);
+  const [missingAddress, setMissingAddress] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("pp");
+
+  const navigate = useNavigate();
+
+  const changeCount = (productID, count) => {
+    reduxDispatch(addToCart(productID, count));
+  };
+
+  const removeFromCartHandler = (productID, quantity, price) => {
+    if (window.confirm("Are you sure?")) {
+      reduxDispatch(removeFromCart(productID, quantity, price));
+    }
+  };
 
   useEffect(() => {
     getUser()
       .then((data) => {
-        setUserAddress({
-          address: data.address,
-          city: data.city,
-          country: data.country,
-          zipCode: data.zipCode,
-          state: data.state,
-          phoneNumber: data.phoneNumber,
-        });
+        if (
+          !data.address ||
+          !data.city ||
+          !data.country ||
+          !data.zipCode ||
+          !data.state ||
+          !data.phoneNumber
+        ) {
+          setButtonDisabled(true);
+          setMissingAddress(
+            " .In order to make order, fill out your profile with correct address, city etc."
+          );
+        } else {
+          setUserAddress({
+            address: data.address,
+            city: data.city,
+            country: data.country,
+            zipCode: data.zipCode,
+            state: data.state,
+            phoneNumber: data.phoneNumber,
+          });
+          setMissingAddress(false);
+        }
+      })
+      .catch((er) =>
+        console.log(
+          er.response.data.message ? er.response.data.message : er.response.data
+        )
+      );
+  }, [userInfo._id]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const orderHandler = () => {
+    const orderData = {
+      orderTotal: {
+        itemsCount: itemsCount,
+        cartSubtotal: cartSubtotal,
+      },
+      cartItems: cartItems.map((item) => {
+        return {
+          productID: item.productID,
+          name: item.name,
+          price: item.price,
+          image: { path: item.image ? item.image.path ?? null : null },
+          quantity: item.quantity,
+          count: item.count,
+        };
+      }),
+      paymentMethod: paymentMethod,
+    };
+    createOrder(orderData)
+      .then((data) => {
+        if (data) {
+          navigate("/user/order-details/" + data._id);
+        }
       })
       .catch((err) => console.log(err));
-  }, []);
+  };
+
+  const choosePayment = (e) => {
+    setPaymentMethod(e.target.value);
+  };
 
   return (
     <Container fluid>
       <Row className="mt-4">
-        <h1>Order Details</h1>
+        <h1>Cart Details</h1>
         <Col md={8}>
           <br />
           <Row>
@@ -44,7 +122,7 @@ const UserOrderDetailsPageComponent = ({ userInfo, getUser }) => {
             </Col>
             <Col md={6}>
               <h2>Payment method</h2>
-              <Form.Select disabled={false}>
+              <Form.Select onChange={choosePayment}>
                 <option value="pp">PayPal</option>
                 <option value="cod">
                   Cash On Delivery (delivery may be delayed)
@@ -55,11 +133,12 @@ const UserOrderDetailsPageComponent = ({ userInfo, getUser }) => {
               <Col>
                 <Alert className="mt-3" variant="danger">
                   Not delivered
+                  {missingAddress}
                 </Alert>
               </Col>
               <Col>
                 <Alert className="mt-3" variant="success">
-                  Paid on 2022-10-02
+                  Not paid yet
                 </Alert>
               </Col>
             </Row>
@@ -67,16 +146,12 @@ const UserOrderDetailsPageComponent = ({ userInfo, getUser }) => {
           <br />
           <h2>Order items</h2>
           <ListGroup variant="flush">
-            {Array.from({ length: 3 }).map((item, idx) => (
+            {cartItems.map((item, idx) => (
               <CartItemComponent
-                item={{
-                  image: { path: "/images/tablets-category.png" },
-                  name: "Product name",
-                  price: 10,
-                  count: 10,
-                  quantity: 10,
-                }}
+                item={item}
                 key={idx}
+                removeFromCartHandler={removeFromCartHandler}
+                changeCount={changeCount}
               />
             ))}
           </ListGroup>
@@ -87,7 +162,8 @@ const UserOrderDetailsPageComponent = ({ userInfo, getUser }) => {
               <h3>Order summary</h3>
             </ListGroup.Item>
             <ListGroup.Item>
-              Items price (after tax): <span className="fw-bold">$892</span>
+              Items price (after tax):
+              <span className="fw-bold">${cartSubtotal}</span>
             </ListGroup.Item>
             <ListGroup.Item>
               Shipping: <span className="fw-bold">included</span>
@@ -96,12 +172,18 @@ const UserOrderDetailsPageComponent = ({ userInfo, getUser }) => {
               Tax: <span className="fw-bold">included</span>
             </ListGroup.Item>
             <ListGroup.Item className="text-danger">
-              Total price: <span className="fw-bold">$904</span>
+              Total price: <span className="fw-bold">${cartSubtotal}</span>
             </ListGroup.Item>
             <ListGroup.Item>
               <div className="d-grid gap-2">
-                <Button size="lg" variant="danger" type="button">
-                  Pay for the order
+                <Button
+                  size="lg"
+                  onClick={orderHandler}
+                  variant="danger"
+                  type="button"
+                  disabled={buttonDisabled}
+                >
+                  Place order
                 </Button>
               </div>
             </ListGroup.Item>
@@ -112,4 +194,4 @@ const UserOrderDetailsPageComponent = ({ userInfo, getUser }) => {
   );
 };
 
-export default UserOrderDetailsPageComponent;
+export default UserCartDetailsPageComponent;
