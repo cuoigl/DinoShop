18b28644b5b4403464201b5c554435e42638b2ae
Cuoi <cuoi@Nguyens-MBP.lan>
2023-07-25 20:38:01 +0700

    API Request For Creating a Product

--- src/pages/admin/AdminCreateProductPage.js
+++ src/pages/admin/AdminCreateProductPage.js
@@ -1,7 +1,34 @@
 import CreateProductPageComponent from "./components/CreateProductPageComponent";
+import axios from "axios";
+
+const createProductApiRequest = async (formInputs) => {
+  const { data } = await axios.post(
+    `http://localhost:3000/api/products/admin`,
+    { ...formInputs },
+    { withCredentials: true }
+  );
+  return data;
+};
+
+const uploadImagesApiRequest = async (images, productId) => {
+  const formData = new FormData();
+  Array.from(images).forEach((image) => {
+    formData.append("images", image);
+  });
+  await axios.post(
+    "http://localhost:3000/api/products/admin/upload?productId=" + productId,
+    formData,
+    { withCredentials: true }
+  );
+};
 
 const AdminCreateProductPage = () => {
-  return <CreateProductPageComponent />;
+  return (
+    <CreateProductPageComponent
+      createProductApiRequest={createProductApiRequest}
+      uploadImagesApiRequest={uploadImagesApiRequest}
+    />
+  );
 };
 
 export default AdminCreateProductPage;
