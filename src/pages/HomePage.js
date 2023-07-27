2b375ee114917dfe04b6aad3ffe3355f4fc0d638
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-28 04:21:24 +0700

    Bestsellers – Make API Request

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,23 @@
 import HomePageComponent from "./components/HomePageComponent";
 import { useSelector } from "react-redux";
+import axios from "axios";
+
+const getBestsellers = async () => {
+  const { data } = await axios.get(
+    "http://localhost:3000/api/products/bestsellers"
+  );
+  return data;
+};
 
 const HomePage = () => {
   const { categories } = useSelector((state) => state.getCategories);
 
-  return <HomePageComponent categories={categories} />;
+  return (
+    <HomePageComponent
+      categories={categories}
+      getBestsellers={getBestsellers}
+    />
+  );
 };
 
 export default HomePage;
