f5a0ed8d7c9b4c3a1dc49a62f427bb8468e10d24
Cuoi <cuoi@Nguyens-MacBook-Pro.local>
2023-07-28 03:54:02 +0700

    Collect id Numbers of Selected Categories

--- src/components/filterQueryResultOptions/CategoryFilterComponent.js
+++ src/components/filterQueryResultOptions/CategoryFilterComponent.js
@@ -1,13 +1,28 @@
 import { Form } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { useRef } from "react";
 
 const CategoryFilterComponent = ({ setCategoriesFromFilter }) => {
   const { categories } = useSelector((state) => state.getCategories);
+  const myRefs = useRef([]);
 
   const selectCategory = (e, category, idx) => {
     setCategoriesFromFilter((items) => {
       return { ...items, [category.name]: e.target.checked };
     });
+
+    var selectedMainCategory = category.name.split("/")[0];
+    var allCategories = myRefs.current.map((_, id) => {
+      return { name: categories[id].name, idx: id };
+    });
+    var indexesOfMainCategory = allCategories.reduce((acc, item) => {
+      var cat = item.name.split("/")[0];
+      if (selectedMainCategory === cat) {
+        acc.push(item.idx);
+      }
+      return acc;
+    }, []);
+    console.log(indexesOfMainCategory);
   };
 
   return (
@@ -18,6 +33,7 @@ const CategoryFilterComponent = ({ setCategoriesFromFilter }) => {
           <div key={idx}>
             <Form.Check type="checkbox" id={`check-api2-${idx}`}>
               <Form.Check.Input
+                ref={(el) => (myRefs.current[idx] = el)}
                 type="checkbox"
                 isValid
                 onChange={(e) => selectCategory(e, category, idx)}
